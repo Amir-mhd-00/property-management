cb336406f0dc704d560fed3a483a6b5f9b8d5ab6
amir mahdi afshar <amirmahdiafshar8@gmail.com>
2026-07-02 08:33:13 -0700

    added role field for users

--- src/main/java/com/example/property_management/entity/UserEntity.java
+++ src/main/java/com/example/property_management/entity/UserEntity.java
@@ -20,6 +20,7 @@ public class UserEntity {
     private Long id;
     private String firstName;
     private String lastName;
+    private String role;
     @Column(unique = true)
     private String email;
     private String phone;
