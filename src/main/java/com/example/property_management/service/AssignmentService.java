98fe5e834e3a0413ceb38eb34b185099261de420
amir mahdi afshar <amirmahdiafshar8@gmail.com>
2026-07-13 21:54:53 -0700

    Update AssignmentService.java

--- src/main/java/com/example/property_management/service/AssignmentService.java
+++ src/main/java/com/example/property_management/service/AssignmentService.java
@@ -1,14 +1,14 @@
 package com.example.property_management.service;
 
+import com.example.property_management.dto.PageResponse;
 import com.example.property_management.dto.assignment.AssignmentDTO;
 import com.example.property_management.dto.assignment.CreateAssignmentRequestDTO;
-
-import java.util.List;
+import org.springframework.data.domain.Pageable;
 
 public interface AssignmentService {
 
     AssignmentDTO createAssignment(CreateAssignmentRequestDTO DTO);
     AssignmentDTO findById(Long id);
-    List<AssignmentDTO> findAll();
+    PageResponse<AssignmentDTO> findAll(Pageable pageable);
     AssignmentDTO end(Long id);
 }
